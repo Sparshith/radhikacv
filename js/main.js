b73f7e017c913b9e4eb47b659a819ebccbf49fbf
Sparshith Sampath <sparshithsampath@gmail.com>
2016-11-06 08:49:48 +0530

    fix js

--- js/main.js
+++ js/main.js
@@ -71,6 +71,38 @@ function main() {
         target: '.navbar-default',
         offset: 80
     })
+
+      $(window).load(function() {   
+       var $container = $('.portfolio-items');   
+       $container.isotope({    
+           filter: '*',    
+           animationOptions: {   
+               duration: 750,    
+               easing: 'linear',   
+               queue: false    
+           }   
+       });   
+       $('.cat a').click(function() {    
+           $('.cat .active').removeClass('active');    
+           $(this).addClass('active');   
+           var selector = $(this).attr('data-filter');   
+           $container.isotope({    
+               filter: selector,   
+               animationOptions: {   
+                   duration: 750,    
+                   easing: 'linear',   
+                   queue: false    
+               }   
+           });   
+           return false;   
+       });   
+   
+   });   
+
+    // $('.work-filter').on('click', function(event) {
+    //     console.log("here");
+    //     event.preventDefault();
+    // });
 	
 
   // jQuery Parallax
